1e2e1e4538981d0661512dadc1983d252a6c3be4
Josh Koter <josh.koter@gmail.com>
2024-12-10 00:18:30 +0200

    Refactor homepage layout and display public key using Web3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
+
 import Web3 from "web3";
 
 export default function Home() {
   const web3 = new Web3(Web3.givenProvider);
 
-  console.info("web3", web3);
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Web 3 stuff I guess</h1>
+    <div className="grid place-items-center min-h-screen p-8 pb-20 gap-16">
+      <main className="text-center">
+        <h1 className="text-3xl mb-4">Web 3 stuff I guess</h1>
+        <p>Your public key is {web3?.ENS?.registryAddress}</p>
       </main>
     </div>
   );
